4a14ea3ed8c568ac52809ffb4d3ea8dee96df643
Abdelrhman Mohamed Allam <abdelrhman.allam@gmail.com>
2020-05-04 11:23:41 +0200

    Update config.php

--- Facebook/login-facebook-sdk/config.php
+++ Facebook/login-facebook-sdk/config.php
@@ -6,6 +6,6 @@
  *
  */
 
-define("FB_APP_ID", "425161951359001");
-define("FB_APP_SECRET", "9bc87f9cc5ef815b0364cf95642c3fce");
+define("FB_APP_ID", "CREATE FB APP");
+define("FB_APP_SECRET", "CREATE FB APP");
 define('FB_REDIRECT_URL', 'http://localhost:8000/fb-callback.php');
